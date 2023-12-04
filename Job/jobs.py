1d7c38a05d9b9a090caff7ee46bd08f1b35ef28b
tuanliaaaa <nhattuan44t@gmail.com>
2023-12-04 19:26:54 +0700

    add Background Job Schedule Alarm

--- Job/jobs.py
+++ Job/jobs.py
@@ -2,16 +2,25 @@ from django.conf import settings
 import requests
 import json
 import random
-from Entity.models import Clock
+from Entity.models import Clock,Equipment
 from django.utils import timezone
-
-
+import requests
+import time
 def schedule_api():
-    pass
-	# current_time = timezone.now()
-	# clockList=Clock.Clock.objects.filter(TimeAction__lte=current_time)
-	# for clock in clockList:
-		
-	# 	clock.delete()
+	while True:
+		try:
+			current_time = timezone.now()
+			clockList=Clock.Clock.objects.filter(TimeAction__lte=current_time)
+			for clock in clockList:
+				equipment = Equipment.Equipment.objects.filter(clocks=clock)[0]
+				equipment.StatusActive= clock.StatusActive
+				equipment.save()
+				url = "http://68.183.236.192/GfqELsw7xlzsGe3hAXnadjsVPxsEiXKe/update/"+equipment.EquipmentKey+"?value="+clock.StatusActive+"&fbclid=IwAR1swiQo5wywsl5hFCw1eIZRc9MkCtlVY0BZ7RgiozCZtp9Pe5Rn_BPtIlk"
+				response = requests.get(url)
+				if response.status_code == 200:
+					clock.delete()
+		except:
+			print("lỗi")
+
 	
 
--- Job/updater.py
+++ Job/updater.py
@@ -1,9 +1,29 @@
 from datetime import datetime
 from apscheduler.schedulers.background import BackgroundScheduler
 from .jobs import schedule_api
-
-
+import threading
+import os
+import csv
+from core.settings import BASE_DIR
 def start():
-	scheduler = BackgroundScheduler()
-	scheduler.add_job(schedule_api, 'interval', seconds=1)
-	scheduler.start()
+    file_path= os.path.join(BASE_DIR,"Job","oke.csv")
+    with open(file_path, 'r', newline='') as csvfile:
+        csv_reader = csv.reader(csvfile)
+        rows = list(csv_reader)
+        print(rows[0][0])
+    if  rows[0][0] == '1':
+        print('done')
+        rows[0][0] = '0'
+        ok= threading.Thread(target=schedule_api)
+        ok.start()
+        with open(file_path, 'w', newline='') as csvfile:
+            csv_writer = csv.writer(csvfile)
+            csv_writer.writerows(rows)
+
+    else:
+        rows[0][0] = '1'
+        with open(file_path, 'w', newline='') as csvfile:
+            csv_writer = csv.writer(csvfile)
+            csv_writer.writerows(rows)
+
+
